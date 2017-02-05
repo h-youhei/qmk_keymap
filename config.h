5443c23985c7c41b2383af4d3ae2a1b593447498
h-youhei <hukumitu.youhei@gmail.com>
2017-02-06 02:26:14 +0900

    divide by consonants and vowles

--- config.h
+++ config.h
@@ -3,4 +3,12 @@
 
 #include "../../config.h"
 
+#undef TAPPING_TERM
+#define TAPPING_TERM 230
+
+#define MOUSEKEY_WHEEL_MAX_SPEED 3
+#define MOUSEKEY_WHEEL_TIME_TO_MAX 0
+
+#define PREVENT_STUCK_MODIFIERS
+
 #endif
--- youhei.h
+++ youhei.h
@@ -10,6 +10,7 @@ enum Layer {
 	L_ARROW,
 	L_NUM,
 	L_FUNCTION,
+	L_CLICK,
 };
 
 // reset layer state
@@ -20,39 +21,102 @@ enum Layer {
 #define ARRW_T(kc) LT(L_ARROW, kc)
 #define NUM_T(kc) LT(L_NUM, kc)
 #define FN_T(kc) LT(L_FUNCTION, kc)
+#define CLCK_T(kc) LT(L_CLICK, kc)
+
+// tapping mod with special key
+#define TRNS_T_LCLCK KC_FN0
+#define TRNS_T_MCLCK KC_FN1
+#define TRNS_T_RCLCK KC_FN2
+#define TRNS_T_LPRN KC_FN3
+#define TRNS_T_RPRN KC_FN4
+#define TRNS_T_LALT KC_FN5
+
+const macro_t *tap_mod_macro(keyrecord_t *record, uint8_t mod, const macro_t *macro) {
+	if (record->event.pressed) {
+		if (record->tap.count <= 0 || record->tap.interrupted) {
+			register_mods(mod);
+		}
+	}
+	else {
+		if (record->tap.count <= 0 || record->tap.interrupted) {
+			unregister_mods(mod);
+		}
+		else {
+			return macro;
+		}
+	}
+	return MACRO_NONE;
+};
+
+const macro_t *tap_layer_macro(keyrecord_t *record, uint8_t layer, const macro_t *macro) {
+	if (record->event.pressed) {
+		if (record->tap.count <= 0 || record->tap.interrupted) {
+			layer_on(layer);
+		}
+	}
+	else {
+		if (record->tap.count <= 0 || record->tap.interrupted) {
+			layer_off(layer);
+		}
+		else {
+			return macro;
+		}
+	}
+	return MACRO_NONE;
+};
+
+void tap_mousekey(uint8_t mouse) {
+	mousekey_on(mouse);
+	mousekey_send();
+	mousekey_off(mouse);
+	mousekey_send();
+};
 
 void tap_mod_mouse(keyrecord_t *record, uint8_t mod, uint8_t mouse) {
-	if(record->event.pressed) {
+	if (record->event.pressed) {
 		// if not tap
-		if(record->tap.count <= 0 || record->tap.interrupted) {
+		if (record->tap.count <= 0 || record->tap.interrupted) {
 			register_mods(mod);
 		}
 	}
 	else {
 		// if not tap
-		if(record->tap.count <= 0 || record->tap.interrupted) {
+		if (record->tap.count <= 0 || record->tap.interrupted) {
 			unregister_mods(mod);
 		}
 		else {
-			mousekey_on(mouse);
-			mousekey_send();
-			mousekey_off(mouse);
-			mousekey_send();
-			record->tap.count = 0;
+			tap_mousekey(mouse);
 		}
 	}
 };
 
+void tap_layer_mouse(keyrecord_t *record, uint8_t layer, uint8_t mouse) {
+	if (record->event.pressed) {
+		if (record->tap.count <= 0 || record->tap.interrupted) {
+			layer_on(layer);
+		}
+	}
+	else {
+		if (record->tap.count <= 0 || record->tap.interrupted) {
+			layer_off(layer);
+		}
+		else {
+			tap_mousekey(mouse);
+		}
+	}
+};
+
+
 void action_function(keyrecord_t *record, uint8_t id, uint8_t opt) {
 	switch(id) {
 		case 0:
-			tap_mod_mouse(record, MOD_LGUI, KC_BTN1);
+			tap_layer_mouse(record, L_CLICK, KC_BTN1);
 			break;
 		case 1:
-			tap_mod_mouse(record, MOD_LALT, KC_BTN2);
+			tap_mod_mouse(record, MOD_LGUI, KC_BTN3);
 			break;
 		case 2:
-			tap_mod_mouse(record, MOD_LCTL, KC_BTN3);
+			tap_mod_mouse(record, MOD_LALT, KC_BTN2);
 			break;
 		default:
 			break;
@@ -63,21 +127,44 @@ const uint16_t PROGMEM fn_actions[] = {
 	[0] = ACTION_FUNCTION_TAP(0),
 	[1] = ACTION_FUNCTION_TAP(1),
 	[2] = ACTION_FUNCTION_TAP(2),
+	[3] = ACTION_MACRO_TAP(1),
+	[4] = ACTION_MACRO_TAP(2),
+	[5] = ACTION_MACRO_TAP(3),
 };
 
 const macro_t *action_get_macro(keyrecord_t *record, uint8_t id, uint8_t opt) {
 	switch(id) {
 		case 0:
-			if(record->event.pressed) {
+			if (record->event.pressed) {
 				layer_clear();
 				clear_mods();
 				clear_oneshot_mods();
 			}
 			break;
+		case 1:
+			return tap_mod_macro(record, MOD_LSFT, MACRO(D(LSFT), T(9), U(LSFT), END));
+			break;
+		case 2:
+			return tap_layer_macro(record, L_CLICK, MACRO(D(LSFT), T(0), U(LSFT), END));
+			break;
+		case 3:
+			return tap_layer_macro(record, L_CLICK, MACRO(D(LALT), T(NO), U(LALT), END));
 		default:
 			break;
 	}
 	return MACRO_NONE;
+};
+
+void matrix_init_user(void) {
+	ergodox_led_all_set(LED_BRIGHTNESS_LO);
+};
+
+void matrix_scan_user(void) {
+	ergodox_led_all_off();
+
+	if (host_keyboard_leds() & (1 << USB_LED_CAPS_LOCK)) {
+		ergodox_right_led_2_on();
+	}
 };
 
 #endif
