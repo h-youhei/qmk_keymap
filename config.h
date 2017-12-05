1f759a054cee7946b1492b079efa20016bb5d0d8
h-youhei <hukumitu.youhei@gmail.com>
2017-12-05 20:56:06 +0900

    move to layouts directory. (qmk/qmk_firmware#1609)

---
+++ config.h
@@ -0,0 +1,13 @@
+#ifndef CONFIG_USER_H
+#define CONFIG_USER_H
+
+#include "../../config.h"
+
+#undef TAPPING_TERM
+#define TAPPING_TERM 200
+
+#define PREVENT_STUCK_MODIFIERS
+
+#define TAPPING_FORCE_HOLD
+
+#endif
