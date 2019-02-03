5256470c3693cf6da5a5fcd5a62607118b0a8fef
Hukumitu Youhei <hukumitu.youhei@gmail.com>
2019-02-03 21:01:39 +0900

    distinguish same hand shift

--- README.md
+++ README.md
@@ -60,18 +60,37 @@ English|[日本語](README.ja.md)
 ## Simultaneous Modifier
 simultaneous tapping version of MOD-TAP.
 
+Key press is treated as simultaneous tapping
+when satisfy both condition below.
+
+- difference of pressed time is within the term defined (`SIMULTANEOUS_WAIT_TERM`)
+- overlapping time the keys are held is over the term defined (`SIMULTANEOUSING_TERM`)
+
 To use it,<br>
 1. copy `process_simultaneous.c/h` and `util_user.c/h`, and append it to SRC in your `rules.mk`
 2. include `process_simultaneous.h` to your `keymap.c`
 3. call `process_simultaneous()` from `process_record_user()`
 4. call `matrix_scan_simultaneous()` from `matrix_scan_user()`
 
+and edit `qmk_firmware/tmk_core/common/action.h`
+```
+typedef struct {
+	...
+	bool simultaneous;
+} keyrecord_t;
+```
+
+or if you don't need to change behavior in `process_record_user`,
+depending on wether it is simultaneous tapping or not,
+remove following code in `(un)register_simultaneous()` in `process_simultaneous.c`
+```
+process_record(...
+	...,
+	.simultaneous = true
+	...
+```
+
 To customize, read process_simultaneous.h .
-
-## Special Keys
-WinLck,CtrLck,AltLck,SftLck are modifier lock like CapsLock.
-
-Clear key is used to unlock modifier lock all at once.
 
 ## details
 IME key is used for toggle Input Method. Internally, it's assigned to KANA key.
--- keymap.c
+++ keymap.c
@@ -5,11 +5,9 @@
 
 enum user_code {
 	KC_P000 = SAFE_RANGE,
-	KC_CTLLK,
-	KC_SFTLK,
-	KC_ALTLK,
-	KC_GUILK,
-	CLEAR
+	CLEAR,
+	// SH stands for Same Hand
+	SH_TRNS,
 };
 
 enum Layer {
@@ -18,9 +16,12 @@ enum Layer {
 	L_STABLE,
 #endif
 	L_FN,
+	L_LSFT,
+	L_RSFT,
 };
-#define FN_T(kc) LT(L_FN, kc)
-#define FN_S(kc) LS(L_FN, kc)
+#define LSFT_LS(kc) LS(L_LSFT, kc)
+#define RSFT_LS(kc) LS(L_RSFT, kc)
+#define FN_LS(kc) LS(L_FN, kc)
 
 // click (left, middle, right)
 #define KC_LCLK KC_BTN1
@@ -29,11 +30,12 @@ enum Layer {
 
 #ifdef ENABLE_STABLE_LAYER
 #define STABLE DF(L_STABLE)
+#define FN_T(kc) LT(L_FN, kc)
 #else
 #define STABLE CLEAR
 #endif
 
-static uint8_t mod_locked = 0;
+
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
 	switch(keycode) {
 	case KC_P000:
@@ -45,9 +47,36 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
 		return false;
 	case CLEAR:
 		if(record->event.pressed) {
-			mod_locked = 0;
-			ergodox_right_led_1_off();
 			clear_mods();
+			layer_clear();
+		}
+		return false;
+	case SH_TRNS:
+		if(record->simultaneous) {
+			if(record->event.pressed) {
+				tap_code(KC_3);
+				uint16_t keycode = keymap_key_to_keycode(0, record->event.key);
+				tap_code(keycode);
+			}
+		}
+		else {
+			uint32_t layers = layer_state | default_layer_state;
+			keypos_t keypos = record->event.key;
+			uint16_t keycode;
+			int8_t i;
+			for(i = L_LSFT - 1; i >= 0; i--) {
+				if(layers & (1UL << i)) {
+					keycode = keymap_key_to_keycode(i, keypos);
+					if(keycode != KC_TRNS) break;
+				}
+			}
+			if(i == -1) keycode = keymap_key_to_keycode(0, keypos);
+			if(record->event.pressed) {
+				register_code(keycode);
+			}
+			else {
+				unregister_code(keycode);
+			}
 		}
 		return false;
 	default:
@@ -72,24 +101,57 @@ void matrix_scan_user(void) {
 
 void led_set_user(uint8_t usb_led) {
 	// I usually set num lock on, I prefer led is on while in unusual state.
-	// BUG:blink the led while suspend
+	// BUG: blink the led while suspend
 	// if(usb_led & (1<<USB_LED_NUM_LOCK)) ergodox_right_led_1_off();
 	// else ergodox_right_led_1_on();
 
-	if(usb_led & (1<<USB_LED_CAPS_LOCK)) ergodox_right_led_2_on();
+	if(IS_LED_ON(usb_led, USB_LED_CAPS_LOCK)) ergodox_right_led_2_on();
 	else ergodox_right_led_2_off();
 
-	if(usb_led & (1<<USB_LED_SCROLL_LOCK)) ergodox_right_led_3_on();
+	if(IS_LED_ON(usb_led, USB_LED_SCROLL_LOCK)) ergodox_right_led_3_on();
 	else ergodox_right_led_3_off();
 }
 
-/* TODO: open this function in upstream. */
+static bool is_shifted = false;
+uint32_t layer_state_set_user(uint32_t state) {
+	uint8_t layer = biton32(state);
+	if(layer == L_LSFT || layer == L_RSFT) {
+		if(!is_shifted) {
+			is_shifted = true;
+			register_code(KC_LSFT);
+		}
+	} else if(layer == L_BASE) {
+		if(is_shifted) {
+			is_shifted = false;
+			unregister_code(KC_LSFT);
+		}
+	}
+	return state;
+}
+
 #ifdef ENABLE_STABLE_LAYER
-void default_layer_state_set_user(uint32_t state) {
-	if(state == L_STABLE) ergodox_right_led_3_on();
-	else ergodox_right_led_3_off();
+uint32_t default_layer_state_set_user(uint32_t state) {
+	uint8_t layer = biton32(state);
+	if(layer == L_STABLE) ergodox_right_led_1_on();
+	else ergodox_right_led_1_off();
+	return state;
 }
 #endif
+
+bool has_simultaneous_priority_to_a(uint16_t keycode_a, uint16_t keycode_b) {
+	if(is_simultaneous_layer(keycode_a)) {
+		uint8_t layer_a = get_simultaneous_layer_from_keycode(keycode_a);
+		if(layer_a == L_LSFT) return true;
+		else if(layer_a == L_RSFT) {
+			if(is_simultaneous_layer(keycode_b)) {
+				uint8_t layer_b = get_simultaneous_layer_from_keycode(keycode_b);
+				return layer_b != L_LSFT;
+			}
+			else return true;
+		}
+	}
+	return false;
+}
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 	[L_BASE] = LAYOUT_ergodox(
@@ -102,7 +164,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 		// thumb
 		KC_RCLK, KC_LCLK,
 		KC_MCLK,
-		LSFT_S(KC_SPC), FN_S(KC_ENT), KC_ESC,
+		LSFT_LS(KC_SPC), FN_LS(KC_ENT), KC_ESC,
 		// right hand
 		KC_NO, KC_8, KC_9, KC_0, KC_6, KC_7, STABLE,
 		KC_INS, KC_K, KC_F, KC_U, KC_Y, KC_Z, KC_HENK,
@@ -112,7 +174,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 		// thumb
 		KC_WBAK, KC_WFWD,
 		KC_WHOM,
-		KC_KANA, FN_S(KC_TAB), RSFT_S(KC_SPC)
+		KC_KANA, FN_LS(KC_TAB), RSFT_LS(KC_SPC)
 		),
 #ifdef ENABLE_STABLE_LAYER
 	[L_STABLE] = LAYOUT_ergodox(
@@ -159,5 +221,49 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 		KC_NO, KC_NO,
 		KC_NO,
 		KC_NO, KC_TRNS, KC_TRNS
+		),
+	[L_LSFT] = LAYOUT_ergodox(
+		// left hand
+		KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_NO,
+		KC_TRNS, SH_TRNS, SH_TRNS, SH_TRNS, SH_TRNS, SH_TRNS, KC_TRNS,
+		SH_TRNS, SH_TRNS, SH_TRNS, SH_TRNS, SH_TRNS, SH_TRNS,
+		KC_TRNS, SH_TRNS, SH_TRNS, SH_TRNS, SH_TRNS, SH_TRNS, KC_TRNS,
+		KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
+		// thumb
+		KC_TRNS, KC_TRNS,
+		KC_TRNS,
+		KC_TRNS, KC_TRNS, KC_TRNS,
+		// right hand
+		KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, DF(L_BASE),
+		KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
+		KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
+		KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
+		KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
+		// thumb
+		KC_TRNS, KC_TRNS,
+		KC_TRNS,
+		KC_TRNS, KC_TRNS, KC_TRNS
+		),
+	[L_RSFT] = LAYOUT_ergodox(
+		// left hand
+		KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
+		KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
+		KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
+		KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
+		KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
+		// thumb
+		KC_TRNS, KC_TRNS,
+		KC_TRNS,
+		KC_TRNS, KC_TRNS, KC_TRNS,
+		// right hand
+		KC_NO, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, STABLE,
+		KC_TRNS, SH_TRNS, SH_TRNS, SH_TRNS, SH_TRNS, SH_TRNS, KC_TRNS,
+		SH_TRNS, SH_TRNS, SH_TRNS, SH_TRNS, SH_TRNS, SH_TRNS,
+		KC_TRNS, SH_TRNS, SH_TRNS, SH_TRNS, SH_TRNS, SH_TRNS, KC_TRNS,
+		KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
+		// thumb
+		KC_TRNS, KC_TRNS,
+		KC_TRNS,
+		KC_TRNS, KC_TRNS, KC_TRNS
 		),
 };
