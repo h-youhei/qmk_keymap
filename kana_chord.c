f66292c446a3495c0b3d0cb2ca63de84183705d7
Hukumitu Youhei <hukumitu.youhei@gmail.com>
2019-09-06 15:09:29 +0900

    fix the bug with shift delay
     in case the keycode is KC_NO on KANA_CHORD_LAYER

--- kana_chord.c
+++ kana_chord.c
@@ -2,7 +2,7 @@
 #include "util_user.h" // in_range
 
 #include "keymap.h" // keymap_key_to_keycode
-#include "action_layer.h" // default_layer_state
+#include "action_layer.h" // default_layer_state, layer_clear
 #include "timer.h" // timer_elapsed
 #include "nodebug.h"
 #include <stddef.h>
@@ -24,38 +24,57 @@ bool process_kana(uint16_t keycode, keyrecord_t *record) {
 			process_kana_chord(keycode, record);
 			return false;
 		}
-		else if(is_kana(keycode)) {
-			if(event.pressed) {
-				if(is_tapping || await_shift_delay) {
-					await_shift_delay = true;
-					is_tapping = false;
-					waiting = event;
-				}
-				else process_record_kana(keycode, record);
+		if(biton32(default_layer_state | layer_state) == L_KANA) {
+			if(is_kana(keycode)) {
+				process_record_kana(keycode, record);
+				return false;
 			}
-			else {
-				if(await_shift_delay) {
-					await_shift_delay = false;
-					process_record(&(keyrecord_t) {
-						.event = waiting
-					});
-				}
-			}
-			return false;
 		}
-		else if(keycode == KANA_ROLL) {
-			is_tapping = false;
-			if(event.pressed) {
-				// first key
-				uint16_t keycode = keymap_key_to_keycode(L_KANA, held_as_layer.key);
-				uint16_t kana = get_kana_from_keycode(keycode);
-				tap_kana(kana, held_as_layer);
-				// second key
-				keycode = keymap_key_to_keycode(L_KANA, event.key);
-				if(is_kana_chord(keycode)) {
-					keycode = get_kana_from_keycode(keycode);
+		else {
+			if(keycode == KANA_ROLL) {
+				is_tapping = false;
+				if(event.pressed) {
+					// first key
+					uint16_t keycode = keymap_key_to_keycode(L_KANA, held_as_layer.key);
+					uint16_t kana = get_kana_from_keycode(keycode);
+					tap_kana(kana, held_as_layer);
+					// second key
+					keycode = keymap_key_to_keycode(L_KANA, event.key);
+					if(is_kana_chord(keycode)) {
+						keycode = get_kana_from_keycode(keycode);
+					}
+					tap_kana(keycode, event);
 				}
-				tap_kana(keycode, event);
+				return false;
+			}
+			else if(is_kana(keycode) || keycode == KC_NO ) {
+				if(event.pressed) {
+					if(await_shift_delay) {
+						await_shift_delay = false;
+						process_record(&(keyrecord_t) {
+							.event = waiting
+						});
+						await_shift_delay = true;
+						waiting = event;
+					}
+					else if(is_tapping) {
+						await_shift_delay = true;
+						is_tapping = false;
+						waiting = event;
+					}
+					else if(is_kana(keycode)) {
+						//test
+						process_record_kana(keycode, record);
+					}
+				}
+				else {
+					if(await_shift_delay) {
+						await_shift_delay = false;
+						process_record(&(keyrecord_t) {
+							.event = waiting
+						});
+					}
+				}
 			}
 			return false;
 		}
